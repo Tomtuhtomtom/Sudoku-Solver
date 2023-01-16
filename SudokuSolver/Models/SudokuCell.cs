01e9be64fc9e0eb21090ed531d75d397b52bcb8b
Thomas Morris <tomchrismorris@yahoo.com>
2023-01-16 17:14:24 -0500

    initial commit-followed from tutorial

---
+++ SudokuSolver/Models/SudokuCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Models
+{
+	public class SudokuCell
+	{
+		public int X { get; set; }
+
+        public int Y { get; set; }
+
+		public bool Solved { get; set; }
+
+		public int Number { get; set; }
+
+		public override string ToString()
+			=> Solved ? Number.ToString() : $"x";
+    }
+}
+
---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,17 @@
+﻿using SudokuSolver.Tools;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Sudoku Solver");
+Console.WriteLine("Instruction: Copy Sudoku line by line with 'x' as empty space.");
+Console.WriteLine("Then watch it solve it!");
+Console.WriteLine("Your Sudoku:");
+
+Solver.Input();
+var solution = string.Join("\n", Solver.Solve());
+
+Console.WriteLine();
+Console.WriteLine("Solution:");
+Console.Write(solution);
+Console.WriteLine();
+Console.WriteLine();
+Console.ReadLine();
